09130c6cedebfb56014d2aaba047738b9c0b8d3d
Zika <70343014+ZikaCodez@users.noreply.github.com>
2025-01-15 10:18:44 +0200

    Add custom error classes for user-related exceptions

---
+++ bitey/core/errors.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Error(commands.CommandError):
+    pass
+
+class UserAlreadyExists(Error): # Attempt of creating a USER that already exists
+    def __init__(self, data):
+        self.data = data
+
+    pass
+
+class UserNotFound(Error): # No data was found for USER
+    pass
