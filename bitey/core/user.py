0749a57e6026049e05e202fb861ae17b71b91d09
Zika <70343014+ZikaCodez@users.noreply.github.com>
2025-01-15 10:47:40 +0200

    Add restaurant existence check and logging during user creation

--- bitey/core/user.py
+++ bitey/core/user.py
@@ -1,11 +1,16 @@
 from database import USERS
 from errors import *
 import datetime
+from utility import *
 
 class User:
     
     async def get_user(self, user_id:int): # Returns the USER data from the database IF FOUND
         data = await USERS.find_one({"_id": str(user_id)})
+        return data
+    
+    async def find_restaurant(self, restaurant_name):
+        data = await USERS.find_one({"restaurant.name": restaurant_name})
         return data
     
     async def check_user_exist(self, user_id): # Raises an error IF user NOT FOUND, otherwise, returns the USER
@@ -16,9 +21,17 @@ class User:
             return user
     
     async def create_user(self, user_id, restaurant_name): # Creates a new USER in the database
+        send_log("PROCESS", f"Creating a new user with ID {user_id} and restaurant name {restaurant_name}")
+        
         user_exist = await self.get_user(user_id)
         if user_exist:
+            send_log("ERROR", f"User with ID {user_id} already exists")
             raise UserAlreadyExists(user_exist)
+        
+        restaurant_exist = await self.find_restaurant(restaurant_name)
+        if restaurant_exist:
+            send_log("ERROR", f"Restaurant name {restaurant_name} is already taken")
+            raise RestaurantNameTaken(restaurant_name)
         
         # Initializing data
         data = {
@@ -54,6 +67,8 @@ class User:
         }
         
         USERS.insert_one(data)
+        
+        send_log("SUCCESS", f"User with ID {user_id} and restaurant name {restaurant_name} has been created")
         
     async def update_user(self, user_id, data):
         user = await self.check_user_exist()
