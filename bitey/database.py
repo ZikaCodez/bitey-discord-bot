c69f53be5e2d25beaa9d92c5dafa02596cdbd5fb
Zika <70343014+ZikaCodez@users.noreply.github.com>
2025-01-15 10:30:04 +0200

    Add boosters collection to database structure

--- bitey/database.py
+++ bitey/database.py
@@ -14,5 +14,6 @@ MAIN_DB = client.MainDB
 # Collections
 USERS = MAIN_DB.users               # users collection
 ITEMS = MAIN_DB.items               # items collection (menu items)
+BOOSTERS = MAIN_DB.boosters         # boosters collection
 INGREDIENTS = MAIN_DB.ingredients   # cooking ingredients
 CHALLENGES = MAIN_DB.challenges     # daily challenges
