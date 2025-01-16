410cd6e960e6f6099caaf6dd6775a5af4bf544e5
Zika <70343014+ZikaCodez@users.noreply.github.com>
2025-01-16 14:39:22 +0200

    Add logging for command synchronization and bot readiness

--- bitey/bot.py
+++ bitey/bot.py
@@ -3,13 +3,18 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
+from core.utility import send_log
 
 # Load environment variables
 load_dotenv()
 
 # Initialize bot
 intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+bot.toothy = "<:toothy:1329004192651677759>"
 
 # Event: When the bot is ready
 @bot.event
@@ -18,6 +23,14 @@ async def on_ready():
     print("------")
 
     # Sync slash commands globally
+    send_log("PROCESS", "Syncing commands...")
+    
+    for filename in os.listdir("./commands"):
+        if filename.endswith(".py"):
+            # Load the command file as a Cog
+            await bot.load_extension(f"commands.{filename[:-3]}")
+            print(f"Loaded command: {filename[:-3]}")
+            
     try:
         synced = await bot.tree.sync()
         print(f"Synced {len(synced)} command(s).")
@@ -25,11 +38,9 @@ async def on_ready():
         print(f"Failed to sync commands: {e}")
 
     # Load commands from the "commands" folder
-    for filename in os.listdir("./commands"):
-        if filename.endswith(".py"):
-            # Load the command file as a Cog
-            await bot.load_extension(f"commands.{filename[:-3]}")
-            print(f"Loaded command: {filename[:-3]}")
+            
+    print("------")
+    send_log("SUCCESS", "Bot is ready")
 
 # Run the bot
 bot.run(os.getenv("DISCORD_TOKEN"))
